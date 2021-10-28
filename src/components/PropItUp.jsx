e986b2abc6d72d04c8409088c10875a1b258361f
Luke Hufnagle <leh17068@gmail.com>
2021-10-28 15:23:16 -0400

    Added a button to increase age by one

--- src/components/PropItUp.jsx
+++ src/components/PropItUp.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class PropItUp extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age : this.props.age
+        };
+    }
     render(){
+        let age = this.props.age;
+        let increaseAge = () =>{
+            this.setState({age : this.state.age +=1})
+        }
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hColor}</p>
+                <button onClick = {increaseAge}>Birthday button for {this.props.firstName}</button>
             <hr />
             </div>
         )
